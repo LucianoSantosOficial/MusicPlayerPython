944c24d3615344c6e38d10d44a508e2804630f4d
Luciano Santos <lucianosantosjob@gmail.com>
2021-11-01 15:17:52 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -7,35 +7,45 @@ class Player:
     def __init__(self):
         
         #JANELA
-        self.window = ThemedTk(theme="black")
+        self.window = ThemedTk(theme="equilux")
         self.window.title("Music Player")
         self.window.resizable(0, 0)
-        self.window.geometry("300x400+500+100")
-        self.window.config(bg="#333333")
+        self.window.geometry("300x410+500+100")
+        self.window.config(bg="#444444")
 
-        self.list = Listbox(self.window, bg="#444444", height=16)
+        self.img_add = PhotoImage(file="assets/add.png")
+        self.img_next = PhotoImage(file="assets/next.png")
+        self.img_pause = PhotoImage(file="assets/pause.png")
+        self.img_play = PhotoImage(file="assets/play.png")
+        self.img_previus = PhotoImage(file="assets/previus.png")
+        self.img_remove = PhotoImage(file="assets/remove.png")
+
+        self.list = Listbox(self.window, bg="#333333", height=14)
         self.list.pack(fill=X, padx=10, pady=10)
 
         self.frame = ttk.Frame(self.window)
         self.frame.pack(pady=10)
+        
+        self.add = ttk.Button(self.frame, image=self.img_add)
+        self.add.grid(row=0, column=0, padx=2)
 
-        self.remove = ttk.Button(self.frame)
-        self.remove.grid(row=0, column=0)
-
-        self.add = ttk.Button(self.frame)
-        self.add.grid(row=0, column=1)
+        self.remove = ttk.Button(self.frame, image=self.img_remove)
+        self.remove.grid(row=0, column=1, padx=2)
 
         self.frame2 = ttk.Frame(self.window)
         self.frame2.pack(pady=10)
 
-        self.previus = ttk.Button(self.frame2)
+        self.previus = ttk.Button(self.frame2, image=self.img_previus)
         self.previus.grid(row=0, column=0)
 
-        self.play = ttk.Button(self.frame2)
+        self.play = ttk.Button(self.frame2, image=self.img_play)
         self.play.grid(row=0, column=1)
 
-        self.next = ttk.Button(self.frame2)
+        self.next = ttk.Button(self.frame2, image=self.img_next)
         self.next.grid(row=0, column=2)
+
+        self.volume = ttk.Scale(self.window)
+        self.volume.pack(fill=X, padx=10)
 
 
         self.window.mainloop()
